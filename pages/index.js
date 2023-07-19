a013e2730ce82f9a8310007fb94c2f79b7485afb
FerreiraArth <122117492+FerreiraArth@users.noreply.github.com>
2023-07-19 21:43:19 +0000

    Adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Erro corrigido! Parabens!</h1>
+function Home() {
+  return <h1>Erro corrigido! Parabens!</h1>;
 }
 
 export default Home;
+
+function teste() {
+  console.log("identação errada");
+}
